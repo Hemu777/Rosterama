feec2b7ce6eaed7273a650df3059713266e05798
Hemu777 <hemu_7777@hotmail.com>
2018-05-17 00:05:00 +1000

    Add files via upload

---
+++ src/main/java/com/hemanthyagnapu/rosterama/MenuScreen.java
@@ -0,0 +1,17 @@
+package com.hemanthyagnapu.rosterama;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hemanthyagnapu on 8/11/16.
+ */
+
+public class MenuScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_screen);
+    }
+}
